7f94eb6aef4f1ea18690a096a2dc1e3f6b79ff98
kalbe-raza <razanaqvi813@gmail.com>
2026-09-10 11:18:52 +0500

    Change normalization logic

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,4 +10,4 @@ def normalize(values):
     maximum = arr.max()
     if minimum == maximum:
         return np.zeros_like(arr)
-    return (arr - minimum) / (maximum - minimum)
+    return arr / maximum
